35d54b94c82f6bd0369201f2e570112678cc0ce2
Tommy413 <tommy199685413@gmail.com>
2016-12-06 14:19:36 +0800

    Simple UI

--- client.cpp
+++ client.cpp
@@ -42,6 +42,47 @@ string recv_msg(int sockfd){
 	return ans;
 }
 
+bool display_option(string* options){
+	int count=atoi(options[0].c_str());
+	cout<<"Choose Request:"<<endl;
+	for (int i = 1; i <= count; ++i)
+	{
+		cout<<i<<". "<<options[i]<<endl;
+	}
+	cout<<"(Please enter the corresponding request index.)"<<endl;
+	return 1;
+}
+
+string make_req(string req,string* options){
+	int choice=atoi(req.c_str());
+	cout<<endl;
+	if (choice > atoi(options[0].c_str())){
+		cout<<"No corresponding request!!!"<<endl;
+		return "error";
+	}
+	string act=options[choice];
+	string args[10];
+
+	if (act=="Register"){
+		cout<<"Account Name?"<<endl;
+		getline(cin,args[0]);
+		return "REGISTER#"+args[0];
+	}
+	else if (act=="Login"){
+		cout<<"Account Name?"<<endl;
+		getline(cin,args[0]);
+		cout<<"Port Number?"<<endl;
+		getline(cin,args[1]);
+		return args[0]+"#"+args[1];
+	}
+	else if (act=="List"){
+		return act;
+	}
+	else if (act=="Logout"){
+		return "Exit";
+	}
+}
+
 bool action(string req,int sockfd){	//0 for success
 	string cmd="";
 	string ans;
@@ -58,15 +99,21 @@ bool action(string req,int sockfd){	//0 for success
 	if (cmd=="Exit"){
 		ans=recv_msg(sockfd);
 		cout<<ans<<endl;
+		if (ans!="bye") return 1;
 		return 0;
 	}
 	else if (cmd=="REGISTER"){
 		ans=recv_msg(sockfd);
 		cout<<ans<<endl;
+		if (ans!="100 OK\n") return 1;
 		return 0;
 	}
 	else if (cmd=="List"){
 		ans=recv_msg(sockfd);
+		if (ans[0]=='2'){
+			cout<<ans<<endl<<endl;
+			return 1;
+		}
 		for (int i = 0; i < ans.size(); ++i)
 		{
 			if (ans[i]=='#')ans[i]='\t';
@@ -75,10 +122,31 @@ bool action(string req,int sockfd){	//0 for success
 		return 0;
 	}
 	else {
-		action("List",sockfd);
-		return 0;
+		return action("List",sockfd);
 	}
 	return 1;
+}
+
+bool post_action(bool fail,string* options,int req_index){	// 0 for continue
+	string req=options[req_index];
+
+	if (fail){
+		cout<<req<<" failed!"<<endl<<endl;
+		display_option(options);
+		return 0;
+	}
+	else {
+		if (req=="Login"){
+			options[1]="List";
+			options[2]="Logout";
+		}
+    	else if (req=="Logout"){
+    		cout<<"Logout : by user request."<<endl<<endl;
+    		return 1;
+    	}
+    	display_option(options);
+    	return 0;
+	}
 }
 
 bool client_socket(const char* ip, int port){	//0 for success
@@ -102,25 +170,36 @@ bool client_socket(const char* ip, int port){	//0 for success
 			return 1;
     	}
 
-    	string req;
+    	string req,reqstr;
     	string ans="test";
+    	bool fail=0;
 
     	ans=recv_msg(c_socket);
     	cout<<ans<<endl;
     	//finish connection
     	
-    	cout<<"Enter your message:"<<endl;
+    	string options[10];
+    	options[0]="2";
+    	options[1]="Register";
+    	options[2]="Login";
+    	display_option(options);
     	while (getline(cin,req)){
-    		send_msg(c_socket,req);
+    		if ((reqstr=make_req(req,options))=="error"){
+    			display_option(options);
+    			continue;
+    		}
+    		send_msg(c_socket,reqstr);
     		sleep(0.25);
-    		cout<<"Output:"<<endl;
-    		action(req,c_socket);
-    		if (req=="Exit"){
+    		cout<<"Output:"<<endl<<endl;
+    		fail=action(reqstr,c_socket);
+    		if (post_action(fail,options,atoi(req.c_str()))){
     			close(c_socket);
-    			cout<<"Logout : by user request."<<endl;
     			break;
     		}
-    		cout<<"Enter your message:"<<endl;
+    	}
+
+    	if (reqstr!="Exit"){
+    		cout<<"Logout : by unknown reason."<<endl;
     	}
 
     	return 0;
@@ -132,8 +211,7 @@ bool client_socket(const char* ip, int port){	//0 for success
 
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	string arg0(argv[0]);
 	if (argc != 3){
 		cout<<"Usage : "<<arg0<<" hostip hostport"<<endl;
